1557c5656bfc94da9b3d6112735cab537bf52626
Alexey Povazhniy <apovazhniy@rocketsoftware.com>
2018-08-02 19:31:30 +0500

    Another step

--- manager.cpp
+++ manager.cpp
@@ -1,8 +1,30 @@
+#include <iostream>
 #include "manager.h"
+
+Module::Module(const char* n, uint32_t a, uint16_t l)
+{
+	name = n;
+	addr = a;
+	length = l;
+	counter = 0;
+}
 
 Bus::Bus(const char* inifile)
 {
-	return;
+// TODO здесь должно быть сделано чтение ini файла и заполнение хэш таблицы Modules
+// так как адреса модулей уникальны то сложность поиска будет O(1)
+// пока заполним конфигурацию "руками"
+
+	Modules.emplace(0x100, Module("RX100", 0x100, 10));
+	Modules.emplace(0x200, Module("RX200", 0x200, 20));
+	Modules.emplace(0x300, Module("RX300", 0x300, 30));
+	Modules.emplace(0x400, Module("RX400", 0x400, 40));
+	Modules.emplace(0x500, Module("RX500", 0x500, 50));
+	Modules.emplace(0x600, Module("RX600", 0x600, 60));
+
+//	for (auto it = Modules.begin(); it != Modules.end(); ++it) std::cout<<it->second.name<<std::endl;
+
+	
 }
 
 int main(int argc, char* argv[])
--- manager.h
+++ manager.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <unordered_map>
+#include <memory>
 #include <string>
 #include <list>
 
@@ -26,30 +27,34 @@ typedef struct Datagram_tag
 //	перед полем с данными, но вероятно это аппаратное ограничение, придётся счётчик "дописывать" в конец данных
 } Datagram;
 
-typedef struct Message_tag
+typedef struct Message_tag // через такие структуры будем принимать извне данные для передачи по шине и отправлять назад ответы
 {
+	uint32_t addr;
 	uint16_t length;
 	char data[];
 } Message;
 #pragma pack(pop)
 
-typedef struct Module_tag
+class Module
 {
-	std::string name;
-	uint32_t addr;
-	uint16_t msglength;
-	uint16_t counter;
-} Module;
+	public:
+		std::string name;	// "имя" модуля, просто текстовая строка
+		uint32_t addr;		// адрес модуля
+		uint16_t length;	// длина сообщения с которым работает модуль
+		uint16_t counter;	// значение счётчика, позволит отслеживать "потерю связи"
+		Module(const char*, uint32_t, uint16_t);
+};
 
 class Bus
 {
 	public:
 		Bus(const char*);
-		std::list<Message*> MessageList;
-//		void SendMessages();
+		Send(std::list <std::shared_ptr<Message> > MessageList);
+//		std::list <std::shared_ptr<Message> > MessageList;
+
 	private:
 		std::unordered_map <uint32_t, Module> Modules;
-		std::list <Telegram> TelegramList;
+		std::list <std::shared_ptr<Telegram> > TelegramList;
 };
 
 #endif
