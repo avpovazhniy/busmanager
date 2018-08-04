8c8bb74c5d6a3400f0cb0a6ace9d5c1adc4e2afe
apovazhniy <apovazhniy@rocketsoftware.com>
2018-08-04 14:12:21 +0600

    Message changed

--- manager.h
+++ manager.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <unordered_map>
+#include <vector>
 #include <memory>
 #include <string>
 #include <list>
@@ -26,35 +27,36 @@ typedef struct Datagram_tag
 //	uint16_t counter; // по условию счётчик расположен именно здесь, это неудачно, лучше бы его расположить
 //	перед полем с данными, но вероятно это аппаратное ограничение, придётся счётчик "дописывать" в конец данных
 } Datagram;
-
-typedef struct Message_tag // через такие структуры будем принимать извне данные для передачи по шине и отправлять назад ответы
-{
-	uint32_t addr;
-	uint16_t length;
-	char data[];
-} Message;
 #pragma pack(pop)
+
+class Message // через такие элементы будем принимать извне данные для передачи по шине и отправлять назад ответы
+{
+	public:
+		Message(uint32_t, std::vector <char>);
+		uint32_t addr;
+		std::vector <char> data;
+};
 
 class Module
 {
 	public:
+		Module(const char*, uint32_t, uint16_t);
 		std::string name;	// "имя" модуля, просто текстовая строка
 		uint32_t addr;		// адрес модуля
 		uint16_t length;	// длина сообщения с которым работает модуль
 		uint16_t counter;	// значение счётчика, позволит отслеживать "потерю связи"
-		Module(const char*, uint32_t, uint16_t);
 };
 
 class Bus
 {
 	public:
 		Bus(const char*);
-		Send(std::list <std::shared_ptr<Message> > MessageList);
-//		std::list <std::shared_ptr<Message> > MessageList;
+		Send(std::list <std::shared_ptr <Message> > MessageList);
+		std::list <std::shared_ptr <Message> > MessageList;
 
 	private:
 		std::unordered_map <uint32_t, Module> Modules;
-		std::list <std::shared_ptr<Telegram> > TelegramList;
+		std::list <std::shared_ptr <Telegram> > TelegramList;
 };
 
 #endif
